08fa2c91caaceb2b59e4b3b3ce414e00dac4df03
Ngao Thien <tinhnhanvh@gmail.com>
2018-06-08 16:41:58 +0700

    update directions map

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,7 @@
 	background: rgba(255, 255, 255, 0.5);
 	box-shadow: 0 2px 6px rgba(0, 0, 0, 0.3);
     width: 400px;
-    display: none;
+    display: block;
 }
 #direct-box .header{
 	background: #3367D6 !important;
@@ -37,10 +37,29 @@
     -webkit-transition: all 0.3s ease-in;
     -o-transition: all 0.3s ease-in;
     transition: all 0.3s ease-in;
+    border-radius: 1px;
 }
 .dismiss:hover {
     background: rgba(255, 255, 255, 0.87);
     color: #4285F4;
+}
+#direct-box .btn-search{
+	background-color: #3367D6;
+	color: rgba(255,255,255,0.87);
+	padding: 10px;
+	text-align: center;
+	font-size: 20px;
+	-webkit-transition: all 0.3s ease-in;
+    -o-transition: all 0.3s ease-in;
+    transition: all 0.3s ease-in;
+}
+#direct-box .btn-search:hover{
+	background-color: rgba(51,103,214,0.87);
+	color: white;
+	cursor: pointer;
+}
+#direct-box #result{
+	background: white;
 }
 </style>
 @endsection
@@ -102,7 +121,7 @@
 			<div class="input-group-prepend">
 				<span class="input-group-text"><i class="fa fa-dot-circle-o"></i></span>
 		    </div>
-			<input type="text" class="form-control" placeholder="Start point..." autocomplete="off" />
+			<input type="text" class="form-control" placeholder="Start point..." autocomplete="off" id="start-point" />
 			<!--<div class="input-group-append">
 				<button type="button" class="btn btn-search"><i class="fa fa-search"></i></button>
 			</div>-->
@@ -111,11 +130,17 @@
 			<div class="input-group-prepend">
 				<span class="input-group-text"><i class="fa fa-map-marker"></i></span>
 		    </div>
-			<input type="text" class="form-control" placeholder="End point..." autocomplete="off" />
+			<input type="text" class="form-control" placeholder="End point..." autocomplete="off" id="end-point" />
 			<!--<div class="input-group-append">
 				<button type="button" class="btn btn-search"><i class="fa fa-search"></i></button>
 			</div>-->
 		</div>
+	</div>
+	<div class="btn-search" id="direct-btn">
+		<i class="fa fa-search"></i>
+	</div>
+	<div id="result">
+		
 	</div>
 </div>
 
@@ -123,7 +148,7 @@
 
 @endsection
 @section('scripts')
-<script async defer src="//maps.googleapis.com/maps/api/js?libraries=places‌​&key=AIzaSyA0kXy7r6QF_I9nixVMeP1TbIZ3ERfWgYc&libraries=places"></script>
+<script async defer src="//maps.googleapis.com/maps/api/js?libraries=places‌​&key=AIzaSyA0kXy7r6QF_I9nixVMeP1TbIZ3ERfWgYc&libraries=places&language=vi&region=vn"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/malihu-custom-scrollbar-plugin/3.1.5/jquery.mCustomScrollbar.concat.min.js"></script>
 <script>
 	$(() => {
@@ -135,6 +160,40 @@
 
 		map.controls[google.maps.ControlPosition.LEFT_TOP].push(document.getElementById('left-panel'));
 		map.controls[google.maps.ControlPosition.LEFT_TOP].push(document.getElementById('direct-box'));
+
+		// auto-complete
+		var directionsService = new google.maps.DirectionsService;
+        var directionsDisplay = new google.maps.DirectionsRenderer;
+        directionsDisplay.setMap(map);
+        directionsDisplay.setPanel(document.getElementById('result'));
+		let startPoint = document.getElementById('start-point');
+		let endPoint = document.getElementById('end-point');
+		var autocompleteFrom = new google.maps.places.Autocomplete(startPoint);
+        var autocompleteTo = new google.maps.places.Autocomplete(endPoint);
+        
+        document.getElementById('direct-btn').addEventListener('click', () => {
+        	directionsService.route({
+        		origin: startPoint.value,
+				destination: endPoint.value,
+				travelMode: google.maps.TravelMode.DRIVING,
+        	}, (response, status) => {
+				if (status === google.maps.DirectionsStatus.OK) {
+					directionsDisplay.setDirections(response);
+					//get direction info
+					var htmlReturn = '';
+					var route = response.routes[0];
+					// console.log(route.legs[0].steps);
+					// var listSteps = route.legs[0].steps;
+					// listSteps.forEach((e) => {
+					// 	document.getElementById('result').innerHTML += e.instructions + '<br>';
+					// });
+					htmlReturn += "Distance: <strong>" + route.legs[0].distance.text + "</strong>, Duration: <strong>" + route.legs[0].duration.text + "</strong>";
+					//document.getElementById('infoDirections').innerHTML  = htmlReturn;
+				} else {
+					window.alert('Directions request failed due to ' + status);
+				}
+        	});
+        });
 
 		$('#direct-box .dismiss').click(() => {
 			$('#direct-box').hide('fast');
