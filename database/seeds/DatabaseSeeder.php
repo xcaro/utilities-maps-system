f9b519d76949c3d8d14a74d17c683ad1765e11bc
Ngao Thien <tinhnhanvh@gmail.com>
2018-07-10 18:58:02 +0700

    add seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -17,5 +17,6 @@ class DatabaseSeeder extends Seeder
          $this->call(PermissionsTableSeeder::class);
          $this->call(RolesTableSeeder::class);
          $this->call(UsersTableSeeder::class);
+         $this->call(SettingsTableSeeder::class);
     }
 }
